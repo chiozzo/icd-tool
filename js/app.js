ed399b0d9703a8fbb58f040ee634669eb29c2e0f
Kirk Walker Graves <graves.kirk@gmail.com>
2016-01-31 14:35:50 -0600

    installed angular ui-router, fleshed out icd-form partial with inputs

--- js/app.js
+++ js/app.js
@@ -2,4 +2,11 @@ var app = angular.module('icd-tool',['ui.router', 'ngMaterial', 'ngMessages']);
 
 app.config(['$stateProvider', '$urlRouterProvider', '$mdThemingProvider', function($stateProvider, $urlRouterProvider, $mdThemingProvider) {
 	$urlRouterProvider.otherwise('/');
+
+	$stateProvider
+
+		.state('form', {
+			url: '/form',
+			templateUrl: 'partials/icd-form.html' 
+		});
 }]);
